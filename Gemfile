4bd94a9da9cb45b9a6480bf4dba94f2144995ebd
easeindesign <easeindesign@gmail.com>
2011-02-05 18:52:04 +0800

    Inital commit, and Page testing CH3 done

--- Gemfile
+++ Gemfile
@@ -1,11 +1,16 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.3'
+gem 'sqlite3-ruby',  :require => 'sqlite3'
 
-gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'autotest'
 
-gem "autotest"
-
-group :test, :development do
-  gem "rspec-rails", "~> 2.4"
+group :development do
+  gem 'rspec-rails'
 end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'webrat'
+end
+
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should have title" do
+      get 'home'
+      response.should have_selector( "title", 
+                   :content => "EID | Home" )
+    end
+    it "should have content in the body tag" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -13,6 +22,23 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    it "should have title" do
+      get 'contact'
+      response.should have_selector( "title", 
+                   :content => "EID | Contact" )
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have title" do
+      get 'about'
+      response.should have_selector( "title", 
+                   :content => "EID | About" )
     end
   end
 
