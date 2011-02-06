22ae8e1ff2d61f4871c2ea2664f285560ff86ce3
easeindesign <easeindesign@gmail.com>
2011-02-06 23:28:13 +0800

    Added users contoller and layout

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
-
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
@@ -22,8 +21,25 @@ describe "LayoutLinks" do
     respond.should have_selector('title', :content => "Help")
   end
 
-end
+  it "should have a Signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    response.should have_selector('title', :content => "Home")
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
+    response.should have_selector('a[href="/"]>img')
+  end
+end
 
 
 #describe "LayoutLinks" do
